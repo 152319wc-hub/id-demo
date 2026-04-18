63cf492604a721ff5fc8c0f85fb1d1071d926d8e
152319wc-hub <152319wc@gmail.com>
2026-04-18 20:06:39 +0800

    Add files via upload
    第一次提交

---
+++ ld/player/states/00PlayerState.gd
@@ -0,0 +1,36 @@
+extends Node
+class_name PlayerState
+
+var player : Player
+var next_state : PlayerState
+
+#region /// state references
+# reference to all other states
+@onready var idle: PlayerStateIdle = %Idle
+@onready var run: PlayerStateRun = %Run
+
+
+#endregion
+
+func init() -> void:
+	#print ("init,",name)
+	pass
+
+
+func enter() -> void:
+	#print ("enter,",name)
+	pass
+	
+func exit() -> void :
+	#print ("exit,",name)
+	pass
+	
+func handle_input(_event : InputEvent) -> PlayerState :
+	return next_state 
+	
+func process(_delta: float ) -> PlayerState:
+	#print(_delta)
+	return next_state
+	
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
